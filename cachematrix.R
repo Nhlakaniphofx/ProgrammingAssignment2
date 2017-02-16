665ae6d60165a6ac372d93281d9d6adf347d0c5a
Nhlakanipho Muholi <fmuholi@gmail.com>
2017-02-16 22:05:48 +0200

    add functionality to catch function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a combination of 2 functions, one which caches an inverse 
+## matrix and the other either surfaces the inverse if cached or computes it
+## if it isn't cached
 
-## Write a short comment describing this function
+## This library has the function "ginv" which calculates the inverse of a matrix
+## even if the matrix is not square
+library(MASS)
+
+## This function caches results of the calculation of a matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  minv <- NULL
+  set <- function(y){
+    x <<- y
+    minv <<- NULL
+  }
+  
+  get <- function() x
+  setinv <- function(inverse) minv <<- inverse
+  getinv <- function() minv
+       list(set = set, get = get,
+            setinv = setinv,
+            getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function calculates get the cached inverse function of a matrix 
+## or calculates it if it's not cached
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  minv <- x$getinv()
+       if(!is.null(minv)) {
+           message("getting cached data")
+           return(minv)
+       }
+       data <- x$get()
+       minv <- ginv(data)
+       x$setinv(minv)
+       minv
 }
